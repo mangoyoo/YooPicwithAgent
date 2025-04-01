33e38395be9089fdfb68cec36748f155c8102b12
mangoyoo <676224061@qq.com>
2025-04-01 21:06:36 +0800

    初始化

---
+++ src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yupicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuPictureBackendApplication.class, args);
+    }
+//test
+}
