dcc0b3a7de19aef1db6fa965723de2209f0bac85
mangoyoo <676224061@qq.com>
2025-04-03 23:10:09 +0800

    开发了注册接口和用户登录接口

---
+++ src/main/java/com/yupi/yupicturebackend/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.yupi.yupicturebackend.controller;
+
+import com.yupi.yupicturebackend.common.BaseResponse;
+import com.yupi.yupicturebackend.common.ResultUtils;
+import com.yupi.yupicturebackend.dto.UserLoginRequest;
+import com.yupi.yupicturebackend.dto.UserRegisterRequest;
+import com.yupi.yupicturebackend.exception.ErrorCode;
+import com.yupi.yupicturebackend.exception.ThrowUtils;
+import com.yupi.yupicturebackend.model.vo.LoginUserVO;
+import com.yupi.yupicturebackend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 用户注册
+     */
+    @PostMapping("/register")
+    public BaseResponse<Long> userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        ThrowUtils.throwIf(userRegisterRequest == null, ErrorCode.PARAMS_ERROR);
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        return ResultUtils.success(result);
+    }
+    @PostMapping("/login")
+    public BaseResponse<LoginUserVO> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(userLoginRequest == null, ErrorCode.PARAMS_ERROR);
+        String userAccount = userLoginRequest.getUserAccount();
+        String userPassword = userLoginRequest.getUserPassword();
+        LoginUserVO loginUserVO = userService.userLogin(userAccount, userPassword, request);
+        return ResultUtils.success(loginUserVO);
+    }
+
+
+
+}
+
---
+++ src/main/java/com/yupi/yupicturebackend/service/UserService.java
@@ -0,0 +1,49 @@
+package com.yupi.yupicturebackend.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupicturebackend.model.entity.User;
+import com.yupi.yupicturebackend.model.vo.LoginUserVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+
+    /**
+     * 获取加密后的密码
+     *
+     * @param userPassword 原始密码
+     * @return 加密后的密码
+     */
+    String getEncryptPassword(String userPassword);
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request httpRequest 请求方便设置 cookie
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+
+}
+
+
